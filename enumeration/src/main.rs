8ef3fcbd76f54f14396e07cb09925edc58d560f8
박성준 <macleod.park@gmail.com>
2025-05-18 13:00:37 +0900

    section 6-1

---
+++ enumeration/src/main.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, PartialEq)]
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+enum Message {
+    StartGame,
+    WinPoint { who: String },
+    ChangePlayerName(String)
+}
+
+// enum Option<T> {
+//     None,
+//     Some(T)
+// }
+fn main() {
+    let red = Color::Red;
+    let green = Color::Green;
+
+    println!("red = {:?}", red);
+    println!("red == green => {}", red == green);
+    println!("red == red => {}", red == Color::Red);
+    
+    let m1 = Message::StartGame;
+    let m2 = Message::WinPoint {
+        who: String::from("hong");
+    };
+    let m3 = Message::ChangePlayerName(String::from("James"));
+    
+    let some_number = Some(2);
+    let absent_number: Option<i32> = None;
+    
+}
